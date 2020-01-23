cf3df5f6fcc3267d1b7566c07b3221fc183f044e
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-01-23 14:40:58 +0300

    Change favicon, title, description, lang, author and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Фронтенд разработчик Марсель Габдулов.`,
+    description: `Марсель Габдулов. Фронтенд разработчик. Cпециализируюсь на React, интересуюсь дизайном`,
+    author: `marsel.gabdulov@gmail.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/android-chrome-512x512.png`, // This path is relative to the root of the site.
       },
     },
     // added
