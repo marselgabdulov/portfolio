bba50e7c6c4d2bf3ea43f3ff84a285a7fa6b13ab
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-01-28 11:50:01 +0300

    Add change language functionality

--- src/state/app.js
+++ src/state/app.js
@@ -1,14 +1,21 @@
 // initial state
 const initialState = {
   isNavPannelOpened: false,
+  isEnglish: false,
 }
 
 // action creator
 const TOOGLE_NAVPANNEL = "TOGGLE_NAVPANNEL"
+const TOOGLE_LANGUAGE = "TOGGLE_LANGUAGE"
 
 export const toggleNavPannel = isNavPannelOpened => ({
   type: TOOGLE_NAVPANNEL,
   isNavPannelOpened,
+})
+
+export const toggleLanguage = isEnglish => ({
+  type: TOOGLE_LANGUAGE,
+  isEnglish,
 })
 
 // reducer
@@ -16,6 +23,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case TOOGLE_NAVPANNEL:
       return { ...state, isNavPannelOpened: action.isNavPannelOpened }
+    case TOOGLE_LANGUAGE:
+      return { ...state, isEnglish: action.isEnglish }
     default:
       return state
   }
