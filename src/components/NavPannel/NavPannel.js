3e5cf823123b96fb22ae4dbb886c11ff54114af6
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-01-28 12:22:44 +0300

    Translate NavPannel

--- src/components/NavPannel/NavPannel.js
+++ src/components/NavPannel/NavPannel.js
@@ -4,27 +4,9 @@ import "./NavPannel.scss"
 import CrossMenu from "../CrossMenu/CrossMenu"
 import { connect } from "react-redux"
 import { toggleNavPannel } from "../../state/app"
+import LangButton from "../LangButton/LangButton"
 
-const links = [
-  {
-    to: "/about",
-    name: "обо мне",
-  },
-  {
-    to: "/projects",
-    name: "работы",
-  },
-  {
-    to: "/edu",
-    name: "учебные работы",
-  },
-  {
-    to: "/resume",
-    name: "резюме",
-  },
-]
-
-function NavPannel({ isNavPannelOpened, dispatch }) {
+function NavPannel({ isNavPannelOpened, isEnglish, dispatch }) {
   return (
     <>
       <div className={isNavPannelOpened ? "nav-opened" : "nav-closed"}>
@@ -38,26 +20,46 @@ function NavPannel({ isNavPannelOpened, dispatch }) {
         </button>
         <div className="nav-pannel__logo">
           <Link to="/" title="вернуться на главную">
-            Марсель Габдулов
+            {isEnglish ? "MG" : "МГ"}
           </Link>
         </div>
-
         <div className="nav-pannel__menu">
-          {links.map((item, index) => (
-            <div className="menu__item" key={index}>
-              <Link
-                to={item.to}
-                onClick={() => dispatch(toggleNavPannel(!isNavPannelOpened))}
-              >
-                {item.name}
-              </Link>
-            </div>
-          ))}
-
+          <LangButton />
+          <div className="menu__item">
+            <Link
+              to="/about"
+              onClick={() => dispatch(toggleNavPannel(!isNavPannelOpened))}
+            >
+              {isEnglish ? "about" : "обо мне"}
+            </Link>
+          </div>
+          <div className="menu__item">
+            <Link
+              to="/projects"
+              onClick={() => dispatch(toggleNavPannel(!isNavPannelOpened))}
+            >
+              {isEnglish ? "projects" : "работы"}
+            </Link>
+          </div>
+          <div className="menu__item">
+            <Link
+              to="/pet-projects"
+              onClick={() => dispatch(toggleNavPannel(!isNavPannelOpened))}
+            >
+              {isEnglish ? "pet projects" : "учебные работы"}
+            </Link>
+          </div>
+          <div className="menu__item">
+            <Link
+              to="/resume"
+              onClick={() => dispatch(toggleNavPannel(!isNavPannelOpened))}
+            >
+              {isEnglish ? "resume" : "резюме"}
+            </Link>
+          </div>
           <div className="nav-pannel__phone">
             <a href="tel:+79166228889">+7 916 622 88 89</a>
           </div>
-
           <div className="nav-pannel__email">
             <a href="mailto:marsel.gabdulov@gmail.com">
               marsel.gabdulov@gmail.com
@@ -93,6 +95,9 @@ function NavPannel({ isNavPannelOpened, dispatch }) {
 }
 
 export default connect(
-  state => ({ isNavPannelOpened: state.app.isNavPannelOpened }),
+  state => ({
+    isNavPannelOpened: state.app.isNavPannelOpened,
+    isEnglish: state.app.isEnglish,
+  }),
   null
 )(NavPannel)
