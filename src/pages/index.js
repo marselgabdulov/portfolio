32180bf88bd923ff0aabfb070eff1e49d1c557fd
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-01-28 12:23:36 +0300

    Translate index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,30 @@ import React from "react"
 import "./styles/index.scss"
 import { Link } from "gatsby"
 import Place from "../assets/place.svg"
+import { connect } from "react-redux"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-function IndexPage() {
+function IndexPage({ isEnglish }) {
   return (
     <Layout>
       <SEO title="Главная" />
       <section className="intro">
-        <h1>Фронтенд разработчик</h1>
-        <p>Специализируюсь на React, интересуюсь дизайном</p>
+        <h1>{isEnglish ? "Frontend developer" : "Фронтенд разработчик"}</h1>
         <p>
-          <Place /> Москва, Россия
+          {isEnglish ? " Specializing in React" : "Специализируюсь на React"}
         </p>
-        <p>Статус: доступен</p>
+        <p>
+          <Place /> {isEnglish ? "Moscow, Russia" : "Москва, Россия"}
+        </p>
+        <p>{isEnglish ? "Status: available" : "Статус: доступен"}</p>
         <div className="intro__link">
-          <Link to="/projects">Мои работы</Link>
+          <Link to="/projects">{isEnglish ? "My projects" : "Мои работы"}</Link>
         </div>
         <p>
           <Link className="intro__contacts" to="/contacts">
-            Контакты
+            {isEnglish ? "Contacts" : "Контакты"}
           </Link>
         </p>
       </section>
@@ -30,4 +33,9 @@ function IndexPage() {
   )
 }
 
-export default IndexPage
+export default connect(
+  state => ({
+    isEnglish: state.app.isEnglish,
+  }),
+  null
+)(IndexPage)
